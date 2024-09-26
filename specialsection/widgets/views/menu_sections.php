e9013583f9f95263f55c603db6777c0bc30f245d
EgorVerh <76529351+EgorVerh@users.noreply.github.com>
2024-09-26 12:59:34 +0300

    Новый widget view

--- specialsection/widgets/views/menu_sections.php
+++ specialsection/widgets/views/menu_sections.php
@@ -1,15 +1,15 @@
 <div class="dropdown">
     <button class="dropdown-button">Меню</button>
     <div class="dropdown-content">
-        <a href="/paid_edu">paid_edu</a>
-        <a href="/grants">grants</a>
-        <a href="/document">document</a>
-        <a href="/common">common</a>
-        <a href="/eduStandarts">eduStandarts</a>
-        <a href="/inter">inter</a>
-        <a href="/budget">budget</a>
-        <a href="/objects">objects</a>
-        <a href="/catering">catering</a>
-        <a href="/education">education</a>
+        <a href="/specialsection/section/paidedu">paid_edu</a>
+        <a href="/specialsection/section/grants">grants</a>
+        <a href="/specialsection/section/document">document</a>
+        <a href="/specialsection/section/common">common</a>
+        <a href="/specialsection/section/edustandarts">eduStandarts</a>
+        <a href="/specialsection/section/inter">inter</a>
+        <a href="/specialsection/section/budget">budget</a>
+        <a href="/specialsection/section/objects">objects</a>
+        <a href="/specialsection/section/catering">catering</a>
+        <a href="/specialsection/section/education">education</a>
     </div>
 </div>
